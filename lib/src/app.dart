7eea39a26d53138351dc434e2f7339f137e36740
Addin Gama Bertaqwa <punyagama@gmail.com>
2019-03-20 09:34:22 +0800

    Break the widget into two separate classes the Widget and the Widget's State

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,9 +1,13 @@
 // Import flutter helper library
 import 'package:flutter/material.dart';
 
+class App extends StatefulWidget {
+
+}
+
 // Create a class that will be our custom widget
 // This class must extend the 'StatelessWidget' base class
-class App extends StatelessWidget {
+class AppState extends State<App> {
   // Must define a 'build' method that returns
   // the widget that *this* widget will show
   @override
