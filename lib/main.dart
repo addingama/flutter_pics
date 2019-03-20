49769d87456ba2defdc813494fd44c6c4a7c4c60
Addin Gama Bertaqwa <punyagama@gmail.com>
2019-03-20 09:26:23 +0800

    Import custom app to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,6 @@
 import 'package:flutter/material.dart';
+import './src/app.dart';
 
 void main() {
-  var app = MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('Lets see some images!'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          print('Hi there!');
-        },
-      ),
-    ),
-  );
-
-  runApp(app);
+  runApp(App());
 }
